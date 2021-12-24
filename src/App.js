f79ee458ffd1a43a1266b8c94e3105402775ca87
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-12-24 10:43:47 -0300

    Iniciando o projeto #VQV

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
-
+// Iniciando o Projeto
 function App() {
   return (
     <div className="App">
